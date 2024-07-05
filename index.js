5fa48169d9a612510ee913251667db117fb96791
AdamNur422 <143763008+AdamNur422@users.noreply.github.com>
2024-07-05 19:28:49 -0400

    switched back to img + granim
    mobile dosent support vid tag

--- index.js
+++ index.js
@@ -12,26 +12,26 @@ typewriter
   .start()
 
 
-// var granimInstance = new Granim({
-//     element: '#canvas-image-blending',
-//     direction: 'top-bottom',
-//     isPausedWhenNotInView: true,
-//     image : {
-//         source: 'assets/atkins.jpg',
-//         blendingMode: 'multiply',
-//     },
-//     states : {
-//         "default-state": {
-//             gradients: [
-//                 ['#29323c', '#485563'],
-//                 ['#FF6B6B', '#556270'],
-//                 ['#80d3fe', '#7ea0c4'],
-//                 ['#f0ab51', '#eceba3']
-//             ],
-//             transitionSpeed: 8000
-//         }
-//     }
-// });
+var granimInstance = new Granim({
+    element: '#canvas-image-blending',
+    direction: 'top-bottom',
+    isPausedWhenNotInView: true,
+    image : {
+        source: 'assets/atkins.jpg',
+        blendingMode: 'multiply',
+    },
+    states : {
+        "default-state": {
+            gradients: [
+                ['#29323c', '#485563'],
+                ['#FF6B6B', '#556270'],
+                ['#80d3fe', '#7ea0c4'],
+                ['#f0ab51', '#eceba3']
+            ],
+            transitionSpeed: 8000
+        }
+    }
+});
 
 // Function to open modals
 function openModal(modalId) {
